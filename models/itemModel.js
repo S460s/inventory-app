e9d1d73ee3885075c7569225e8005b5b34e78e7a
S460 <aleksandar.ta6ev@gmail.com>
2021-07-17 13:22:48 +0300

    Added item model and schema

--- models/itemModel.js
+++ models/itemModel.js
@@ -4,4 +4,15 @@ const Schema = mongoose.Schema;
 const ItemSchema = new Schema({
 	name: { type: String, required: true },
 	description: { type: String, required: true },
+	category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+	password: { type: string, required: true },
+	price: { type: Number, required: true },
+	numberInStock: { type: Number, required: true },
+	contact: { type: string },
 });
+
+ItemSchema.virtual('url').get(function () {
+	return '/item/' + this._id;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
