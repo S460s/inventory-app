fe38591476ffaedd72504683f8357f6077cf645c
S460 <aleksandar.ta6ev@gmail.com>
2021-07-19 18:07:25 +0300

    Added message if there are no categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,9 @@
+const Category = require('../models/categoryModel');
+
 const category_list = (req, res, next) => {
-	res.render('categories/category_list.pug');
+	const categories = Category.find().then((category_list) => {
+		res.render('categories/category_list.pug', { category_list });
+	});
 };
 
 module.exports = {
