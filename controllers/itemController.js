422c1e5bc7bccea45e774ba246af660f1657cb69
S460 <aleksandar.ta6ev@gmail.com>
2021-07-20 15:08:55 +0300

    added validation for item form inputs

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,9 @@
+const objPromiseAll = require('obj-promise-all');
+const { body, validationResult } = require('express-validator');
+
 const Item = require('../models/itemModel');
 const Category = require('../models/categoryModel');
+
 const item_details = (req, res, next) => {
 	const id = req.params.id;
 	Item.findById(id)
@@ -18,6 +22,25 @@ const item_create_get = (req, res, next) => {
 		})
 		.catch(next);
 };
+
+const item_create_post = [
+	body('pswd', 'Password is required.').trim().escape().isLength({ min: 1 }),
+	body('name', 'Name is required.').trim().escape().isLength({ min: 1 }),
+	body('description', 'Description is required.')
+		.trim()
+		.escape()
+		.isLength({ min: 1 }),
+	body('price', 'Price is required.').trim().escape().isLength({ min: 1 }),
+	body('numberInStock', 'Number-in-Stock is required.')
+		.trim()
+		.escape()
+		.isLength({ min: 1 }),
+	body('contact', 'Contact is required.').trim().escape().isLength({ min: 1 }),
+	body('imgUrl', 'Image URL is required.').trim().escape().isLength({ min: 1 }),
+
+	,
+	(req, res, next) => {},
+];
 
 module.exports = {
 	item_details,
